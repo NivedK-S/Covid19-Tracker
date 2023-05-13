b2c2a165888b9e44f81ba5245bb09e2fe93a3102
Nived K S <chippukichu10@gmail.com>
2023-05-13 18:09:37 +0530

    List of states added

--- src/Components/Statedata.js
+++ src/Components/Statedata.js
@@ -3,26 +3,22 @@ import React,{Component} from 'react';
 import { Accordion,Card, Button } from 'react-bootstrap';
 
 class Statedata extends Component{
-    constructor(){
-        super();
-        this.state={
-           stateData : {}
-        }
-    }
-    componentDidMount(){
-        Axios.get("https://data.covid19india.org/state_district_wise.json").then(response=>{
-              response.data
-              this.setState({stateData:response.data});
-        });
-    }
+    // constructor(){
+    //     super();
+    //     this.state={
+    //        stateData : {}
+    //     }
+    // }
+    // componentDidMount(){
+    //     Axios.get("https://data.covid19india.org/state_district_wise.json").then(response=>{
+    //           response.data
+    //           this.setState({stateData:response.data});
+    //     });
+    // }
     render(){
         return(
-            <div className='row'>
-                <div className='col-md-12'>
-        
-
-                </div>
-
+            <div >
+               List of states in India 
             </div>
         )
     }
