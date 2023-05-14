56967f6328d36972bab93ee92a8f7b41207c1173
Nived K S <chippukichu10@gmail.com>
2023-05-14 19:56:58 +0530

    Tables Added

--- src/Components/Example.js
+++ src/Components/Example.js
@@ -1,43 +1,38 @@
+import React,{Component} from 'react';
 import Accordion from 'react-bootstrap/Accordion';
-import { useAccordionButton } from 'react-bootstrap/AccordionButton';
-import Card from 'react-bootstrap/Card';
 
-function CustomToggle({ children, eventKey }) {
-  const decoratedOnClick = useAccordionButton(eventKey, () =>
-    console.log('totally custom!'),
-  );
-
-  return (
-    <button
-      type="button"
-      style={{ backgroundColor: 'pink' }}
-      onClick={decoratedOnClick}
-    >
-      {children}
-    </button>
-  );
-}
-
-function Example() {
-  return (
-    <Accordion defaultActiveKey="0">
-      <Card>
-        <Card.Header>
-          <CustomToggle eventKey="0">Click me!</CustomToggle>
-        </Card.Header>
-        <Accordion.Collapse eventKey="0">
-          <Card.Body>Hello! I'm the body</Card.Body>
-        </Accordion.Collapse>
-      </Card>
-      <Card>
-        <Card.Header>
-          <CustomToggle eventKey="1">Click me!</CustomToggle>
-        </Card.Header>
-        <Accordion.Collapse eventKey="1">
-          <Card.Body>Hello! I'm another body</Card.Body>
-        </Accordion.Collapse>
-      </Card>
+class Statedata extends Component{
+    render(){
+        return(
+            <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
     </Accordion>
-  );
+
+        )
+    }
 }
-export default Example;
+
+export default Statedata;
--- src/Components/Statedata.js
+++ src/Components/Statedata.js
@@ -1,16 +1,63 @@
-import { Axios } from 'axios';
 import React,{Component} from 'react';
-import { Accordion,Card, Button } from 'react-bootstrap';
-import Example from './Example';
+import Accordion from 'react-bootstrap/Accordion';
+import axios from 'axios';
 
 class Statedata extends Component{
-  
+    constructor(){
+        super();
+        this.state={
+            stateData : {}
+        }
+    }
+    componentDidMount(){
+        axios.get("https://data.covid19india.org/state_district_wise.json").then(Response=>{
+          Response.data
+          this.setState({stateData:Response.data})
+        });
+    }
     render(){
+        let keys=Object.keys(this.state.stateData);
         return(
-            <div >
-               <Example/>
-            </div>
+            <div className='row'>
+                <div className='col-md-12'>
+            <Accordion defaultActiveKey="0">
+                {
+                    keys.map((itm,ky)=>{
+                      let districts= this.state.stateData[itm].districtData;
+                      let district_keys = Object.keys(districts);
+                      
+                        return(
+                            <Accordion.Item eventKey="0">
+        <Accordion.Header>{itm}</Accordion.Header>
+        <Accordion.Body>
+         <table className='table table-bordered table-striped'>
+            <thead>
+                <tr>
+                    <td>District</td>
+                    <td>Confirmed</td>
+                    <td>Active</td>
+                    <td>Recovered</td>
+                    <td>Deaths</td>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+         </table>
+        </Accordion.Body>
+      </Accordion.Item>
+
+                      )    
+                    })
+                }
+
+      
+    </Accordion>
+    </div>
+    </div>
+
         )
     }
 }
+
 export default Statedata;
